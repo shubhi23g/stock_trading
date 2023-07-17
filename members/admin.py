7b5b763883fa807005afc9a8d0ea3966c79ba2a1
shubhi23g <136715268+shubhi23g@users.noreply.github.com>
2023-07-17 11:27:37 +0530

    Update admin.py

--- members/admin.py
+++ members/admin.py
@@ -2,6 +2,4 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(UserProfile)
-# class FileAdmin(admin.ModelAdmin):
-#     list_display = ["id", "company", "closing_prize", "date"]
 admin.site.register(StockTrading)
